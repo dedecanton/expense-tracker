5f12f0d9d78e80670f8c8b191a3efc77681501c2
dedecanton <andrecanton18@gmail.com>
2021-11-20 17:58:53 -0300

    show date completed

--- src/components/TableItem/styles.ts
+++ src/components/TableItem/styles.ts
@@ -5,3 +5,17 @@ export const TableLine = styled.tr``;
 export const TableColumn = styled.td`
     padding: 10px 0;
 `;
+
+export const Category = styled.div<{ color: string }>`
+    display: inline-block;
+    padding: 5px 10px;
+    border-radius: 5px;
+    color: #FFF;
+    background: ${props => props.color};
+`
+
+export const Value = styled.div<{ color:string }>`
+    color:${props => props.color};
+    padding:10px;
+
+`
